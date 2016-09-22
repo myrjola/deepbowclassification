4e3a053dd7e49ba04d4d0506ff600c556ada9952
Martin Yrjölä <martin.yrjola@gmail.com>
2016-09-22 11:17:25 +0200

    Yay! matcaffe is classifying images!

---
+++ readAndPreprocessImageVGG.m
@@ -0,0 +1,13 @@
+function Iout = readAndPreprocessImageVGG(filename)
+  im_data = caffe.io.load_image(filename);
+  IM_WIDTH = 224;
+  IM_HEIGHT = 224;
+  im_data = imresize(im_data, [IM_WIDTH, IM_HEIGHT]);
+  im_data = single(im_data);
+  if size(im_data, 3) == 1
+      % greyscale image doesn't have BGR channels.
+      im_data = repmat(im_data, [1 1 3]);
+  end
+  % Just repeats the image 10 times. This is pretty ineffective.
+  Iout = repmat(im_data, [1 1 1 10]);
+end
--- very_deep_classification.m
+++ very_deep_classification.m
@@ -18,9 +18,34 @@ im_data_repeated = cat(4, im_data_repeated, ones(224, 224, 3));
 whos im_data;
 whos im_data_repeated;
 
-%% Test the net on one image
+%% Test the net
 res = net.forward({im_data_repeated});
 
+res = activationVgg(net, trainingSet, 'fc8');
+
+% Get training labels from the trainingSet
+trainingLabels = trainingSet.Labels;
+
+% Train multiclass SVM classifier using a fast linear solver, and set
+% 'ObservationsIn' to 'columns' to match the arrangement used for training
+% features.
+classifier = fitcecoc(res, trainingLabels, ...
+    'Learners', 'Linear', 'Coding', 'onevsall', 'ObservationsIn', 'columns');
+
+testFeatures = activationVgg(net, testSet, 'fc8');
+
+% Pass CNN image features to trained classifier, for some reason a transpose
+% was needed.
+predictedLabels = predict(classifier, testFeatures');
+
+% Get the known labels
+testLabels = testSet.Labels;
+
+% Tabulate the results using a confusion matrix.
+confMat = confusionmat(testLabels, predictedLabels);
+
+% Convert confusion matrix into percentage form
+confMat = bsxfun(@rdivide,confMat,sum(confMat,2))
 
 %% Get weights and output
 weights = net.layers('fc8').params(1).get_data();
